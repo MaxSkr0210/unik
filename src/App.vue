f7c61889a518b4f2560d4a76f206968446d1bfe7
MaxSkr0210 <71515588+MaxSkr0210@users.noreply.github.com>
2022-06-18 22:33:04 +0300

    edit app

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,16 @@
 </template>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Roboto:wght@300;500;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap");
 
 * {
-  font-family: "Roboto", sans-serif;
+  font-family: "Montserrat", sans-serif;
   font-size: 16px;
 }
 
 body {
   margin: 0;
   padding: 0;
-  font-family: "Roboto", sans-serif;
 }
 
 .btn {
